7c98f5403106864f6fb62d24c601121e05870b8d
Edison <aalevenegass@gmail.com>
2024-02-06 12:41:47 -0300

    Añadiendo fondo en contenedor principal

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Navbar></Navbar>
-    </>
+      <h1 className="container__title">Bienvenido maestro Pokemon!</h1>
+    </div>
   );
 }
 
