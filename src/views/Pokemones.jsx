231046aee9d19d898ee63ec261b7d02683d7785f
Edison <aalevenegass@gmail.com>
2024-02-06 19:42:49 -0300

    Creando contenido de vista pokemones

--- src/views/Pokemones.jsx
+++ src/views/Pokemones.jsx
@@ -1,7 +1,7 @@
 const Pokemones = () => {
   return (
-    <div>
-      <h1>Hola</h1>
+    <div className="pokemon__container">
+      <h1 className="Pokemon__title">Selecciona un Pokemon</h1>
     </div>
   );
 };
